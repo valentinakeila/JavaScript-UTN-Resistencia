c2a5d4b80807e8abd2b5708947da6b54f16187a5
Valen <valentinakeila12@gmail.com>
2023-10-06 12:41:59 -0300

    commit

---
+++ Unidad 6 js utn/funciones_evaluacion6_garrido.js
@@ -0,0 +1,17 @@
+function Login() {
+    let ingreso = document.getElementById ('calcular').value ;
+    let resultado = document.getElementById('calcular').value * 0.21;
+    let total = document.getElementById ('calcular').value * 1.21
+
+    document.write ("El número ingresado es: " + ingreso + ", el IVA de este es: " +resultado + " y el total del número ingresado con el IVA es: " + total)
+    
+    
+    
+}
+ 
+
+
+
+
+
+
---
+++ unidad 2 utn elearning/recetario.js
@@ -0,0 +1,15 @@
+/* archivo para realizar el ejercicio */
+var ingrediente1 = prompt("Ingrese un ingrediente");
+	
+	if(ingrediente1 == "sal"){
+		}
+
+var ingrediente2 = prompt("Ingrese un segundo ingrediente");
+	
+	if(ingrediente2 == "aceite"){
+    }
+
+var ingrediente3 = prompt("Ingrese un tercer ingrediente");
+	
+	if(ingrediente3 == "aceto"){
+    }
